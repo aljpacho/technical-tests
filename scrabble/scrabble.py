d48ee9daf699606438457ef5c61ece67f6104832
Jordan Pacho <trainee.jordan.pacho@sigmalabs.co.uk>
2022-08-22 12:07:34 +0100

    notes for game flow

---
+++ scrabble/scrabble.py
@@ -0,0 +1,16 @@
+"""
+Scrabble game
+
+1) create bag with tiles
+2) shuffle tiles
+3) Assign tiles to rack
+4) Players go
+    - user input
+    - get score if valid
+    -return player word and score
+    -return longest possible word from chars in rack
+    -return highest scoring word from chars in rack
+5) Remove used tiles from bag and rack
+**repeat steps**
+6) If bag is empty and the player has attempted a word 3 times --> check if they want to continue or end game
+"""
